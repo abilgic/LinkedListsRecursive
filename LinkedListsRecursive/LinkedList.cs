0ae02be386cea62634b05cc05f3ef7591d5092a7
ahmet bilgic <ahmet_bilgic@yahoo.com>
2024-07-05 14:51:55 +0300

    Add project files.

---
+++ LinkedListsRecursive/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsRecursive
+{
+    public class LinkedList
+    {
+        public Node root;
+
+        public class Node
+        {
+            public int Data;
+            public Node(int Data) { this.Data = Data; }
+            public Node next;
+        }
+
+        public Node Insert(Node head, int Data)
+        {
+            if (head == null)
+            {
+                var newnode = new Node(Data);
+                head = newnode;
+                return newnode;
+            }
+            else
+            {
+                head.next = Insert(head.next, Data);
+                return head;
+
+            }
+        }
+
+        public void Print(Node head)
+        {
+            if (head == null)
+                return;
+            Console.Write($"{head.Data} ");
+            Print(head.next);
+        }
+    }
+
+
+}
---
+++ LinkedListsRecursive/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var linkedlist = new LinkedListsRecursive.LinkedList();
+var head = linkedlist.root;
+
+head = linkedlist.Insert(head, 1);
+head = linkedlist.Insert(head, 2);
+head = linkedlist.Insert(head, 3);
+head = linkedlist.Insert(head, 4);
+head = linkedlist.Insert(head, 5);
+
+Console.WriteLine("After adding elements to the Linked List");
+linkedlist.Print(head);
+Console.WriteLine();
